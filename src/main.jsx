d2a67322b048f67b3a1b8ca3b4dea128a0bce305
RikiCatte <riccardocatelani2005@gmail.com>
2026-05-15 11:41:33 +0200

    - aggiunto routing react in modo da poter scorrere direttamente alla sezione portfolio tramite l'endpoint '/portfolio' - nuova dipendenza di progetto nei package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './styles/tokens.css'
 import './styles/base.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </StrictMode>,
 )
